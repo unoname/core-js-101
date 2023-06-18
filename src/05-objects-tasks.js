c3f4ac97d5f7d2e0d960800f9f2ca2238de065b4
aleks_pv <apanov08@gmail.com>
2023-06-18 11:06:30 +0400

    feat: add 05

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,11 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.getArea = () => (width * height);
 }
 
 
@@ -35,8 +38,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +54,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 
@@ -110,36 +113,116 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  selector: '',
+  elementCount: false,
+  idCount: false,
+  classCount: false,
+  attrCount: false,
+  pseudoClassCount: false,
+  pseudoElementCount: false,
+
+  element(value) {
+    const newSelector = Object.create(this);
+    const {
+      elementCount,
+      idCount,
+      classCount,
+      attrCount,
+      pseudoClassCount,
+      pseudoElementCount,
+    } = this;
+    if (idCount
+      || classCount
+      || attrCount
+      || pseudoClassCount
+      || pseudoElementCount
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    if (elementCount) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    newSelector.selector = `${this.selector}${value}`;
+    newSelector.elementCount = true;
+    return newSelector;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    const newSelector = Object.create(this);
+    const {
+      idCount,
+      classCount,
+      attrCount,
+      pseudoClassCount,
+      pseudoElementCount,
+    } = this;
+    if (classCount
+      || attrCount
+      || pseudoClassCount
+      || pseudoElementCount
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    if (idCount) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    newSelector.selector = `${this.selector}#${value}`;
+    newSelector.idCount = true;
+    return newSelector;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    const newSelector = Object.create(this);
+    const {
+      attrCount,
+      pseudoClassCount,
+      pseudoElementCount,
+    } = this;
+    if (attrCount
+      || pseudoClassCount
+      || pseudoElementCount
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    newSelector.selector = `${this.selector}.${value}`;
+    newSelector.classCount = true;
+    return newSelector;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    const newSelector = Object.create(this);
+    const {
+      pseudoClassCount,
+      pseudoElementCount,
+    } = this;
+    if (pseudoClassCount
+      || pseudoElementCount
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    newSelector.selector = `${this.selector}[${value}]`;
+    newSelector.attrCount = true;
+    return newSelector;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    const newSelector = Object.create(this);
+    const { pseudoElementCount } = this;
+    if (pseudoElementCount) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    newSelector.selector = `${this.selector}:${value}`;
+    newSelector.pseudoClassCount = true;
+    return newSelector;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    const newSelector = Object.create(this);
+    const { pseudoElementCount } = this;
+    if (pseudoElementCount) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    newSelector.selector = `${this.selector}::${value}`;
+    newSelector.pseudoElementCount = true;
+    return newSelector;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const newSelector = Object.create(this);
+    newSelector.selector = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return newSelector;
+  },
+
+  stringify() {
+    return this.selector;
   },
 };
-
 
 module.exports = {
   Rectangle,
